bd645fb26bb6050507bb7c5b96ca9c40d7bdb449
javed <javed@gems.techverx.com>
2017-08-11 06:49:59 +0500

    navigation bar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-$(document).foundation()
+$(document).foundation();
+
+
+$(document).ready(function() {
+	$('.trigger-srch').click(function() {
+		$('.menu-over-area .search-main').slideToggle();
+	});
+	$('.trigger-nav').click(function() {
+		$('.right-nav .list-unstyled').slideToggle();
+	});
+});
+
