9980d29be40e4d5397c22cd01f9f052bdd1e99b1
javed <javed@gems.techverx.com>
2017-08-21 23:54:23 +0500

    changes to html

--- js/app.js
+++ js/app.js
@@ -8,5 +8,30 @@ $(document).ready(function() {
 	$('.trigger-nav').click(function() {
 		$('.right-nav .list-unstyled').slideToggle();
 	});
-});
+	
+
+	// $(window).resize(function() {
+ 	
+	// });
+
+}); /*document ready ends*/
+
+
+	/*sticky navigation*/
+	
+	$(window).scroll(function() {
+    if ($(this).scrollTop() > 112) 
+    	{
+        $('.sticky-container').addClass('stick-top');
+    	}
+    else
+    	{
+			$('.sticky-container').removeClass('stick-top');
+		}
+
+	});
+
+
+		
+
 
